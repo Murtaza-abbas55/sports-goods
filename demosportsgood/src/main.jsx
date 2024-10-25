26be461180550211177b25255f42fad834f73657
Murtaza Abbas <114948398+Murtaza-abbas55@users.noreply.github.com>
2024-10-25 13:51:06 +0500

    Delete demosportsgood directory

--- demosportsgood/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Change this import
-import App from './App';
-
-// Get the root element from the HTML file
-const rootElement = document.getElementById('root');
-
-// Create the root and render the App component
-ReactDOM.createRoot(rootElement).render(<App />);
