f6861583f37ba9ff596392463203c6aeae128ab6
Murtaza-Abbas55 <murtaza.abbas824@gmail.com>
2024-11-08 11:01:06 +0500

    User authentication done

---
+++ backend/middlewares/uservalidation.js
@@ -0,0 +1,26 @@
+import { body, validationResult } from 'express-validator';
+
+export const validateSignup = [
+    body('user_id').notEmpty().withMessage('User ID is required'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be at least 6 characters'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+export const validateLogin = [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').notEmpty().withMessage('Password is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
