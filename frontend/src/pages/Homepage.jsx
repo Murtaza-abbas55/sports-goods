2c4d7f9f0a213241f8b76eb09976611d52fb6978
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-11-11 13:23:59 +0500

    wrap inside Box

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import ImageCarousel from "../components/ImageCarousel";
 import DrawerAppBar from "../components/Navbar";
 import ImgMediaCard from "../components/Card";
@@ -8,26 +8,50 @@ function Header() {
         <>
             <DrawerAppBar />
             <ImageCarousel />
-            <Typography
-                variant="h4"
-                fontWeight={"bold"}
-                align="center"
-                margin={"16px"}
-            >
-                New Arrival
-            </Typography>
-            <Stack
-                direction={"row"}
-                gap={2}
-                flexWrap={"wrap"}
-                justifyContent={"center"}
-            >
-                <ImgMediaCard name={"Basketball"} price={2000} />
-                <ImgMediaCard name={"Basketball"} price={2000} />
-                <ImgMediaCard name={"Basketball"} price={2000} />
-                <ImgMediaCard name={"Basketball"} price={2000} />
-                <ImgMediaCard name={"Basketball"} price={2000} />
-            </Stack>
+            <Box sx={{ margin: "1.5rem" }}>
+                <Typography
+                    variant="h4"
+                    fontWeight={"bold"}
+                    align="center"
+                    margin={"16px"}
+                >
+                    New Arrival
+                </Typography>
+                <Stack
+                    direction={"row"}
+                    gap={2}
+                    flexWrap={"wrap"}
+                    justifyContent={"center"}
+                >
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                </Stack>
+            </Box>
+            <Box sx={{ margin: "2rem" }}>
+                <Typography
+                    variant="h4"
+                    fontWeight={"bold"}
+                    align="center"
+                    margin={"16px"}
+                >
+                    Trending
+                </Typography>
+                <Stack
+                    direction={"row"}
+                    gap={2}
+                    flexWrap={"wrap"}
+                    justifyContent={"center"}
+                >
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                    <ImgMediaCard name={"Basketball"} price={2000} />
+                </Stack>
+            </Box>
         </>
     );
 }
