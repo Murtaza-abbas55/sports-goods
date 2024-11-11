bb8484119bae86c534dd67fc21829bd560126625
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-11-11 13:50:18 +0500

    Make section component for new arrival and trending

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -2,56 +2,15 @@ import { Box, Stack, Typography } from "@mui/material";
 import ImageCarousel from "../components/ImageCarousel";
 import DrawerAppBar from "../components/Navbar";
 import ImgMediaCard from "../components/Card";
+import Section from "../components/Section";
 
 function Header() {
     return (
         <>
             <DrawerAppBar />
             <ImageCarousel />
-            <Box sx={{ margin: "1.5rem" }}>
-                <Typography
-                    variant="h4"
-                    fontWeight={"bold"}
-                    align="center"
-                    margin={"16px"}
-                >
-                    New Arrival
-                </Typography>
-                <Stack
-                    direction={"row"}
-                    gap={2}
-                    flexWrap={"wrap"}
-                    justifyContent={"center"}
-                >
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                </Stack>
-            </Box>
-            <Box sx={{ margin: "2rem" }}>
-                <Typography
-                    variant="h4"
-                    fontWeight={"bold"}
-                    align="center"
-                    margin={"16px"}
-                >
-                    Trending
-                </Typography>
-                <Stack
-                    direction={"row"}
-                    gap={2}
-                    flexWrap={"wrap"}
-                    justifyContent={"center"}
-                >
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                    <ImgMediaCard name={"Basketball"} price={2000} />
-                </Stack>
-            </Box>
+            <Section sectionHeading={"New Arrival"} />
+            <Section sectionHeading={"Trending"} />
         </>
     );
 }
