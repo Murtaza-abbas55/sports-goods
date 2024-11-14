21e9e1b33b02eebe746088cf5e0ffbab93781c47
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-11-14 11:03:52 +0500

    remove conditional rendering

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -3,19 +3,16 @@ import { useAuth } from "../context/AuthContext";
 function AdminHome() {
     const { Data } = useAuth();
     return (
-        <div>
-            {Data ? (
-                <div>
-                    <p>Welcome, {Data.admin_username}!</p>
-                    <p>Email: {Data.email}</p>
-                    <p>Role: {Data.isAdmin ? "Admin" : "User"}</p>
-                </div>
-            ) : (
-                <p>Not logged in.</p>
-            )}
+        <>
+            <div>
+                <p>Welcome, {Data.admin_username}!</p>
+                <p>Email: {Data.email}</p>
+                <p>Role: {Data.isAdmin ? "Admin" : "User"}</p>
+            </div>
+
             <h1>Admin Homepage</h1>
             <Link to={"/"}>Home</Link>
-        </div>
+        </>
     );
 }
 export default AdminHome;
