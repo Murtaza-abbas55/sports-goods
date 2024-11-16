9a91b30e314664e96e385b0f4128011dfa453abe
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-11-16 21:08:36 +0500

    allow guest to add products in cart

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
 // src/context/AuthContext.js
 import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
 const AuthContext = createContext();
 
@@ -10,6 +12,23 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [Data, setData] = useState(null);
+
+    useEffect(() => {
+        const mergeAnonymousCart = async (user_id) => {
+            try {
+                const response = await axios.post("/api/merge", {
+                    user_id: user_id,
+                });
+                console.log("Cart merge response:", response.data.cartId);
+                // setCartID(response.data.cartId);
+                console.log("Data");
+                console.log(Data.user_id);
+            } catch (error) {
+                console.error("Error merging anonymous cart:", error);
+            }
+        };
+        if (Data) mergeAnonymousCart(Data.user_id);
+    }, [Data]);
 
     const login = (data) => {
         setIsAuthenticated(true);
