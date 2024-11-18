c6444b0e97fd0201b277c65ed8b0cae0353fdd7c
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-11-18 13:08:59 +0500

    add Modal redirecting to login page

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -13,6 +13,20 @@ import FavoriteBorderRoundedIcon from "@mui/icons-material/FavoriteBorderRounded
 import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
 import { useAuth } from "../context/AuthContext";
 import { useEffect } from "react";
+import Modal from "@mui/material/Modal";
+import { Link as RouterLink } from "react-router-dom";
+
+const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+};
 
 function ListingCard({
     product_id,
@@ -29,6 +43,10 @@ function ListingCard({
     const [toastOpen, setToastOpen] = useState(false);
     const [toastMessage, setToastMessage] = useState("");
     const [wishlistStatus, setWishlistStatus] = useState(false);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         const checkWishlistStatus = () => {
@@ -131,25 +149,7 @@ function ListingCard({
     }
 
     function handleLogin() {
-        // <Modal
-        //     open={open}
-        //     onClose={handleClose}
-        //     aria-labelledby="modal-modal-title"
-        //     aria-describedby="modal-modal-description"
-        // >
-        //     <Box sx={style}>
-        //         <Typography id="modal-modal-title" variant="h6" component="h2">
-        //             Text in a modal
-        //         </Typography>
-        //         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        //             Duis mollis, est non commodo luctus, nisi erat porttitor
-        //             ligula.
-        //         </Typography>
-        //     </Box>
-        // </Modal>;
-        console.log("please login");
-        console.log(Data);
-        console.log(isAuthenticated);
+        setOpen(true);
     }
 
     return (
@@ -275,6 +275,31 @@ function ListingCard({
                 message={toastMessage}
                 anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
             />
+
+            {/* Active when guest tries to use wishlist */}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                    >
+                        Login to add items to wishlist
+                    </Typography>
+                    <Button
+                        component={RouterLink}
+                        to={"/login"}
+                        variant="contained"
+                    >
+                        Log In
+                    </Button>
+                </Box>
+            </Modal>
         </Card>
     );
 }
