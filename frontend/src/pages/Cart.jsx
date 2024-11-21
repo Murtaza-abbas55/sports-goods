7d1a14e91cba66c5c0a8d897ffb549a13a545b3e
ZEEFAN12345 <zeefmanzoor527@gmail.com>
2024-11-21 18:50:04 +0500

    set layout of cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,14 +1,28 @@
+import { Box, Divider, Stack, Typography } from "@mui/material";
 import DrawerAppBar from "../components/Navbar";
 import useFetchCartItems from "../hooks/useFetchCartItems";
+import useFetchProductByID from "../hooks/useFetchProductByID";
 
 function Cart() {
     const { cartProducts, setCartProducts } = useFetchCartItems();
+
+    console.log("cart page");
     console.log(cartProducts);
     return (
         <>
             <DrawerAppBar />
-            <h1>Cart Page</h1>
-            <h1>My Cart</h1>
+            <Typography textAlign={"center"} component={"h1"} variant="h5">
+                SHOPPING CART
+            </Typography>
+            <Divider />
+            <Stack direction={"row"}>
+                <Stack flex={0.75}>
+                    <Box>P1</Box>
+                    <Box>P2</Box>
+                    <Box>P3</Box>
+                </Stack>
+                <Stack></Stack>
+            </Stack>
         </>
     );
 }
