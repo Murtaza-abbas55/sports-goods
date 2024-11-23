8f2d1fdbd188248fa19fb597d559c497384a663f
ZEEFAN12345 <zeefmanzoor527@gmail.com>
2024-11-23 19:17:15 +0500

    feature for users to delete review

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import { AddReviewController,DeleteReviewController,GetReviewController,getAllProductReviewsController } from '../controllers/ReviewsController.js';
-import {verifyUser} from '../middlewares/userauth.js'
+import express from "express";
+import {
+    AddReviewController,
+    DeleteReviewController,
+    GetReviewController,
+    getAllProductReviewsController,
+} from "../controllers/ReviewsController.js";
+import { verifyUser } from "../middlewares/userauth.js";
 const router = express.Router();
-router.post('/addreview',verifyUser,AddReviewController);
-router.post('/deleteReview',verifyUser,DeleteReviewController);
-router.get('/reviews',verifyUser,GetReviewController);
-router.get('/reviews/:id',getAllProductReviewsController);
+router.post("/addreview", verifyUser, AddReviewController);
+router.post("/deleteReview", verifyUser, DeleteReviewController);
+router.get("/reviews", verifyUser, GetReviewController);
+router.get("/reviews/:product_id", getAllProductReviewsController);
 export default router;
--- frontend/src/components/Reviews.jsx
+++ frontend/src/components/Reviews.jsx
@@ -1,54 +1,169 @@
 import ListItem from "@mui/material/ListItem";
 import List from "@mui/material/List";
-import { useState } from "react";
-import { Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Button, Divider, Stack, Typography } from "@mui/material";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
+import Rating from "@mui/material/Rating";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
 
 function Reviews({ product_id }) {
-    const [reviews, setReviews] = useState([
-        {
-            review_id: 1,
-            rating: 5,
-            comments: "Ordered it today it was very good",
-            user_id: "4344",
-        },
-        {
-            review_id: 2,
-            rating: 5,
-            comments: "Ordered it today it was very good",
-            user_id: "4344",
-        },
-        {
-            review_id: 3,
-            rating: 5,
-            comments: "Ordered it today it was very good",
-            user_id: "4344",
-        },
-        {
-            review_id: 4,
-            rating: 5,
-            comments: "Ordered it today it was very good",
-            user_id: "4344",
-        },
-    ]);
+    const { Data } = useAuth();
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`/api/reviews/${product_id}`);
+                setReviews(response.data.data.reviews);
+                console.log("Fetching product reviews");
+                console.log(response.data.data.reviews);
+            } catch (error) {
+                console.error("Error fetching product review:", error);
+            }
+        };
+
+        fetchData();
+    }, [product_id]);
+    console.log(reviews);
+
+    async function postReview(product_id) {
+        try {
+            const response = await axios.post("/api/addreview", {
+                product_id,
+                user_id: Data.user_id,
+                rating: 4,
+                comments: "review sent from frontend",
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(
+                "Error while adding product to cart:",
+                error.response?.data || error.message
+            );
+        } finally {
+            setReviews((prevReviews) => [
+                ...prevReviews,
+                {
+                    user_name: Data.user_name,
+                    user_id: Data.user_id,
+                    rating: 4,
+                    comments: "review sent from frontend",
+                },
+            ]);
+        }
+    }
+
+    // async function postReview(product_id) {
+    //     const newReview = {
+    //         product_id,
+    //         user_id: Data.user_id,
+    //         rating: 4,
+    //         comments: "review sent from frontend",
+    //     };
+
+    //     try {
+    //         const response = await axios.post("/api/addreview", newReview);
+    //         console.log(response.data);
+    //     } catch (error) {
+    //         console.error(
+    //             "Error while adding product review:",
+    //             error.response?.data || error.message
+    //         );
+    //         alert("Failed to post the review. Please try again.");
+    //     } finally {
+    //         setReviews((prevReviews) => [...prevReviews, newReview]);
+    //     }
+    // }
+
+    async function deleteReview(product_id, user_id) {
+        try {
+            const response = await axios.post("/api/deleteReview", {
+                product_id,
+                user_id,
+            });
+            console.log(response.data);
+            setReviews((prevReviews) =>
+                prevReviews.filter((review) => review.user_id !== user_id)
+            );
+        } catch (error) {
+            console.error(
+                "Error while deleting review cart:",
+                error.response?.data || error.message
+            );
+        }
+    }
+    console.log("reviews");
     console.log(reviews);
 
     return (
         <>
-            <h1>Reviews</h1>
-            <p>This is Review of {product_id}</p>
-            <List>
-                {reviews.map((review) => (
-                    <ListItem key={review.review_id}>
-                        <Stack>
-                            <Typography variant="h5">
-                                {review.user_id}
-                            </Typography>
-                            <Typography>{review.rating}</Typography>
-                            <Typography>{review.comments}</Typography>
-                        </Stack>
-                    </ListItem>
-                ))}
-            </List>
+            <Divider />
+            <Typography textAlign={"center"} variant="h4">
+                Reviews
+            </Typography>
+            {reviews.length === 0 ? (
+                <h1>Empty</h1>
+            ) : (
+                <>
+                    <List>
+                        {reviews.map((review) => (
+                            <ListItem divider key={review.user_id}>
+                                <Stack>
+                                    <Typography variant="h5">
+                                        {review.user_name}
+                                    </Typography>
+                                    <Stack gap={1} direction={"row"}>
+                                        <Rating
+                                            name="read-only"
+                                            value={review.rating}
+                                            readOnly
+                                        />
+                                        <Typography fontWeight={"bold"}>
+                                            {review.rating + ".0"}
+                                        </Typography>
+                                    </Stack>
+                                    <Typography
+                                        color="text.secondary"
+                                        marginTop={1}
+                                    >
+                                        {review.comments
+                                            .charAt(0)
+                                            .toUpperCase() +
+                                            review.comments.slice(1)}{" "}
+                                    </Typography>
+                                    {Data?.user_id === review.user_id && (
+                                        <Stack marginTop={3} direction={"row"}>
+                                            <IconButton>
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton
+                                                onClick={() =>
+                                                    deleteReview(
+                                                        product_id,
+                                                        Data.user_id
+                                                    )
+                                                }
+                                            >
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </Stack>
+                                    )}
+                                </Stack>
+                            </ListItem>
+                        ))}
+                    </List>
+                </>
+            )}
+            <Button
+                variant="contained"
+                sx={{ margin: "10px 10px" }}
+                onClick={() => postReview(product_id)}
+            >
+                Review Product
+            </Button>
         </>
     );
 }
