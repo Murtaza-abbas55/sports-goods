aa75bdd51e6ba9bd56da96d8c6e5e4dd15e71086
Murtaza-Abbas55 <murtaza.abbas824@gmail.com>
2024-12-02 21:53:43 +0500

    new changes

--- backend/controllers/CategoriesController.js
+++ backend/controllers/CategoriesController.js
@@ -1,4 +1,4 @@
-import { getAllCategories,getProductsByCategoryId } from "../models/Categories.js";
+import { getAllCategories,getProductsByCategoryId,insertCategory,updateCategory } from "../models/Categories.js";
 
 export const AllCategories = async (req,res)=>{
    try{
@@ -19,4 +19,35 @@ export const ProductsByCategory = async (req, res) => {
     catch(err) {
      res.status(500).json({ error: err.message });
     }
+};
+export const addCategoryController = async (req, res) => {
+  try {
+    const { category_id, name, description } = req.body;
+
+    if (!category_id || !name || !description) {
+      return res.status(400).json({ message: "All fields are required: category_id, name, description" });
+    }
+
+    const newCategory = await insertCategory(category_id, name, description );
+    res.status(201).json({ message: "Category added successfully", category: newCategory });
+  } catch (error) {
+    console.error("Error in addCategoryController:", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const {category_id, name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({ message: "Both name and description are required for update" });
+    }
+
+    const updatedCategory = await updateCategory(category_id, name, description );
+    res.status(200).json({ message: "Category updated successfully", category: updatedCategory });
+  } catch (error) {
+    console.error("Error in updateCategoryController:", error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,8 +1,10 @@
-import { AllCategories,ProductsByCategory } from "../controllers/CategoriesController.js";
+import { AllCategories,ProductsByCategory,addCategoryController,updateCategoryController } from "../controllers/CategoriesController.js";
 import express from "express";
+import verifyAdmin from "../middleware/verifyAdmin.js";
 const router= express.Router();
 
 router.get('/categories',AllCategories);
 router.get('/category-products/:id',ProductsByCategory);
-
+router.post('/add-category',verifyAdmin,addCategoryController);
+router.post('/udate-category',verifyAdmin,updateCategoryController);
 export default router;
